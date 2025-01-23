f688d361a5e20fbb3427109df0edbb77d505ae42
ninameier1 <s1081087@student.windesheim.nl>
2025-01-23 11:50:16 +0100

    Update profile settings

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,4 +1,4 @@
-<nav x-data="{ open: false }" class="bg-primary dark:bg-dark border-b border-darkneutral dark:border-secondary sticky">
+<nav x-data="{ open: false }" class="bg-primary dark:bg-dark border-b border-secondary dark:border-secondary sticky">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
@@ -18,10 +18,10 @@
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('bustickets.create')" :active="request()->routeIs('bustickets')">
-                        {{ __('Book Bustickets') }}
+                        {{ __('Plan Your Trip') }}
                     </x-nav-link>
                 </div>
-                <button id="darkModeToggle" class="bg-neutral dark:bg-darkneutral p-2 rounded">
+                <button id="darkModeToggle" class="bg-neutral dark:bg-darkneutral p-2 rounded hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     Toggle Dark Mode
                 </button>
             </div>
