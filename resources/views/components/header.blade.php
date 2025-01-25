ec493035ffab3dbb9f16345fcaf595a1a86db582
ninameier1 <s1081087@student.windesheim.nl>
2025-01-25 16:21:15 +0100

    Update design

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,5 +1,5 @@
-<header class="bg-white dark:bg-dark shadow">
-    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+<header class="bg-white dark:bg-secondary shadow">
+    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 text-2xl text-secondary dark:text-dark">
         {{ $slot }} <!-- Fill it up -->
     </div>
 </header>
